2350182a11f17bdb1780ce4d900caca601227063
axtrough <lolmyfriend14@gmail.com>
2024-08-22 00:42:20 +0200

    Item registry and 2 imported items from old mod

---
+++ src/main/java/net/raccoon/will/willsworkshop/item/WWItemRegistry.java
@@ -0,0 +1,18 @@
+package net.raccoon.will.willsworkshop.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.raccoon.will.willsworkshop.WillsWorkshop;
+
+public class WWItemRegistry {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(WillsWorkshop.MODID);
+
+    public static final DeferredItem<Item> ENZORITE = ITEMS.register("enzorite", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_ENZORITE = ITEMS.register("raw_enzorite", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+    ITEMS.register(eventBus);
+    }
+}
