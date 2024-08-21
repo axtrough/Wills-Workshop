02d8ced9ab647626c480a72c2e5a91b6cf93619e
axtrough <lolmyfriend14@gmail.com>
2024-08-22 00:41:52 +0200

    Block registry and 3 blocks from old mod

---
+++ src/main/java/net/raccoon/will/willsworkshop/block/WWBlockRegistry.java
@@ -0,0 +1,42 @@
+package net.raccoon.will.willsworkshop.block;
+
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.raccoon.will.willsworkshop.WillsWorkshop;
+import net.raccoon.will.willsworkshop.item.WWItemRegistry;
+
+import java.util.function.Supplier;
+
+
+public class WWBlockRegistry {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(WillsWorkshop.MODID);
+
+    public static final DeferredBlock<Block> ENZORITE_BLOCK = BLOCKS.register("enzorite_block", () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.COPPER)));
+    public static final DeferredBlock<Block> RAW_ENZORITE_BLOCK = BLOCKS.register("raw_enzorite_block", () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.RAW_IRON_BLOCK).sound(SoundType.ANCIENT_DEBRIS)));
+    public static final DeferredBlock<Block> DEEPSLATE_ENZORITE_ORE = BLOCKS.register("deepslate_enzorite_ore", () -> new DropExperienceBlock(UniformInt.of(3, 7), BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE).strength(4.5f).requiresCorrectToolForDrops()));
+
+
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn =BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        WWItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
